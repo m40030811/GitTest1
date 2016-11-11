bdac47e4f037b9d8625c05119f563b41ad16fa99
Minjee Kim <m40030811@gmail.com>
2016-11-11 14:17:40 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,6 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
 		System.out.println("안녕하세요~~"); 
+        System.out.println("modified this file...");
 	}
 
 }
